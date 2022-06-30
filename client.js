8a5f09109a19c95e3490cffe781961346d1a898f
Hathaway-Shan <hathaway.shan@gmail.com>
2022-06-30 15:32:35 -0700

    added RLS build table in supabase added URL  and key to client.js in root

---
+++ client.js
@@ -0,0 +1,6 @@
+//links to my supabase url and table
+const SUPABASE_URL = 'https://naecyvnfmwcpxcyroblz.supabase.co';
+const SUPABASE_KEY =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5hZWN5dm5mbXdjcHhjeXJvYmx6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTY2MjM4MDQsImV4cCI6MTk3MjE5OTgwNH0.etgq6aLYRaVsRE4OXn1toSNWnUhRvFKalDJSmm9q4mk';
+
+export const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
